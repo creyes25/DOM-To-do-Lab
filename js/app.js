478b44442181767e75286478a380caa67ca940f4
creyes25 <carolina.reyes2022@gmail.com>
2022-08-30 19:20:24 -0500

    Finish lab

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// cached elements
+const inputElement = document.querySelector('#input')
+const buttonElement = document.querySelector('#submit-button')
+const ulElement = document.querySelector('#todo-list')
+
+// event listner to button when clicked
+buttonElement.addEventListener('click', (evt) => {
+  // create a new <li> element
+  const li = document.createElement('li')
+  // set text to <li> to text entered in <input>
+  li.textContent = inputElement.value
+  console.log(inputElement.value)
+  // reset text in <input> field to an empty string
+  inputElement.value = ''
+  ulElement.appendChild(li)
+})
